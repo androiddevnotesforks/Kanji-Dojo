f78f5aba1c69cfa8f577e8b5d77f257e71422719
syt0r <shuliak.yaroslav@gmail.com>
2025-02-24 01:13:55 +0200

    Fix urls not opening on Mac

--- core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/common/ComposeUrlJvm.kt
+++ core/src/jvmMain/kotlin/ua/syt0r/kanji/presentation/common/ComposeUrlJvm.kt
@@ -2,13 +2,19 @@ package ua.syt0r.kanji.presentation.common
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import java.awt.Desktop
+import java.net.URI
 
 @Composable
 actual fun rememberUrlHandler(): UrlHandler {
     return remember {
         object : UrlHandler {
             override fun openInBrowser(url: String) {
-                ProcessBuilder("xdg-open", url).start()
+                if (Desktop.isDesktopSupported()) {
+                    Desktop.getDesktop().browse(URI.create(url))
+                } else {
+                    ProcessBuilder("xdg-open", url).start()
+                }
             }
         }
     }
