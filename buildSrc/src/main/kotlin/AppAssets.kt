7ae64b3b51d21348902db03233d570d559ff2594
syt0r <shuliak.yaroslav@gmail.com>
2025-08-04 04:04:36 +0300

    Update app data database asset url to v15

--- buildSrc/src/main/kotlin/AppAssets.kt
+++ buildSrc/src/main/kotlin/AppAssets.kt
@@ -26,7 +26,7 @@ object AppAssets {
         expectedAssets = listOf(
             Asset(
                 fileName = AppDataAssetFileName,
-                url = "https://github.com/syt0r/Kanji-Dojo-Data/releases/download/v14.0/kanji-dojo-data-base-v14.sql"
+                url = "https://github.com/syt0r/Kanji-Dojo-Data/releases/download/v15.0/kanji-dojo-data-base-v15.sql"
             ),
             Asset(
                 fileName = "text_analysis_preview.json",
